c0dac41e54e5a5cdbdc24d31ff47b6625572943a
Jeya <jeyabalaji.subramanian@gmail.com>
2019-12-30 10:50:31 +0530

    tested with auto generated version

--- models/models.py
+++ models/models.py
@@ -1,9 +1,12 @@
 from sqlalchemy import *
-from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
-                            backref)
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    relationship,
+    backref
+)
 from sqlalchemy.ext.declarative import declarative_base
 from db_utils.db_session_maker import get_db_session
-
 
 db_session = get_db_session()
 
@@ -14,18 +17,33 @@ Base.query = db_session.query_property()
 
 class Department(Base):
     __tablename__ = 'department'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer)
     name = Column(String)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('id'),
+        {},
+    )
 
 
 class Employee(Base):
     __tablename__ = 'employee'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer)
     name = Column(String)
-    hired_on = Column(DateTime, default=func.now())
-    department_id = Column(Integer, ForeignKey('department.id'))
+    hired_on = Column(DateTime)
+    department_id = Column(Integer)
+
     department = relationship(
         Department,
-        backref=backref('employees',
+        backref=backref('employee',
                         uselist=True,
                         cascade='delete,all'))
+
+    __table_args__ = (
+        PrimaryKeyConstraint('id'),
+        ForeignKeyConstraint(
+            (department_id,),
+            [Department.id]
+        ),
+        {},
+    )
